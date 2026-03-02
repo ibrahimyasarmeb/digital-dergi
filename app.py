baa2ceedf1c197a5cd4289b13452e5b9d0118c0c
ibrahimyasarmeb <ibrahimyasarmeb@gmail.com>
2026-03-02 12:23:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1350,6 +1350,7 @@ def admin():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 1000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
